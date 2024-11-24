c5736665d7a6cb52f53b94b2d3510aef4c32361a
Sophie <sophsong@student.ubc.ca>
2024-11-24 17:51:22 -0500

    Light Mode Computer Version Complete

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -1,34 +1,124 @@
 // import logo from './logo.svg';
+import React, { useRef } from 'react';
 import './App.css';
+import FloatingToolbar from './FloatingToolbar';
+import Briefcase from './assets/briefcase-icon.svg';
+import GradCap from './assets/graduation-cap-icon.svg';
+import GitHub from './assets/github-icon.svg';
+import LinkedIn from './assets/linkedin-icon.svg';
+import Camera from './assets/camera-icon.svg';
+import TokyoTower from './assets/tokyo-tower.jpg'
+import EiffelTower from './assets/eiffel-tower.jpg'
+import SophieLightMode from './assets/sophie-light-mode.jpg'
+import Arcadis from './assets/arcadis-logo.svg';
+import Vinquery from './assets/vinquery-logo.svg';
+import UBC from './assets/ubc-logo.svg';
+import EndLight from './assets/end-page-light.png';
+
+
+function toggleMenu() {
+  const toolbar = document.querySelector('.toolbar');
+  const isVisible = toolbar.style.display === 'flex';
+  toolbar.style.display = isVisible ? 'none' : 'flex';
+}
 
 function App() {
+  const header1Ref = useRef(null);
+  const header2Ref = useRef(null);
+  const header3Ref = useRef(null);
+
+  const headerRefs = {
+    header1: header1Ref,
+    header2: header2Ref,
+    header3: header3Ref,
+  };
+  
+  const scrollToHeader = (headerId) => {
+    const targetRef = headerRefs[headerId];
+    if (targetRef && targetRef.current) {
+      targetRef.current.scrollIntoView({
+        behavior: 'smooth', // Smooth scrolling
+        block: 'start',     // Align to the top
+      });
+    } else {
+      console.error(`No ref found for header: ${headerId}`);
+    }
+  };
+
   return (
     <div className="container">
-      <div className="background">
+      <FloatingToolbar onScrollToHeader={scrollToHeader}/>
+      <div className="mt-fuji-background">
         <div class="center-container">
           <header className="Name-title">
             Sophie Song
           </header>
         </div>
       </div>
-      
+      <h1 ref={header1Ref}>Who <em>are </em> you?</h1>
+        <div className="container-about-me">
+          <div className="container-about-me-inner">
+            <img src={Briefcase} alt="Briefcase, representing current occupation"/>
+            <p>Data Scientist @ Arcadis</p>
+            <img src={GradCap} alt="Graduation cap, representing education attained"/>
+            <p>B.Sc in Computational Intelligence and Design @ UBC <br/> (Cognitive Science & Computer Science)</p>
+            <img className="container-about-me-pic" src={SophieLightMode} alt="A picture of me at TeamLab Planets"/>
+          </div>
+          <img className="container-about-me-tower" src={TokyoTower} alt="Picture of the Tokyo Tower I took"/>
+          <div className="container-about-me-inner">
+            <img src={GitHub} alt="GitHub icon"/>
+            <a href="https://github.com/misophie" target="_blank" rel="noopener noreferrer">GitHub</a>
+            <img src={LinkedIn} alt="LinkedIn icon"/>
+            <a href="https://www.linkedin.com/in/sophie--song/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+            <img src={Camera} alt="Camera icon"/>
+            <a href="../assets/photos.pdf" target="_blank">Photography</a>
+          </div>
+        </div>
+        <h1 ref={header2Ref}>Experience & Education</h1>
+        <div className="container-experience-education">
+          <img src={Arcadis} alt="Arcadis Logo"/>
+          <div className="container-experience-right">
+            <p class="experience-title"> Data Scientist</p>
+            <p class="experience-subtitle">Nov. 2024 - Present</p>
+          </div>
+          <img src={Vinquery} alt="Vinquery Logo"/>
+          <div className="container-experience-right">
+            <p class="experience-title"> Software Developer Intern</p>
+            <p class="experience-subtitle">May 2020 - Nov. 2024</p>
+          </div>
+          <img src={UBC} alt="University of British Columbia Logo"/>
+          <div className="container-experience-right">
+            <p class="experience-title"> Computational Intelligence and Design (B.Sc)</p>
+            <p class="experience-subtitle">Sep. 2019 - May 2024</p>
+          </div>
+        </div>
+        <h1 ref={header3Ref}>Projects</h1>
+        <div className="container-projects">
+          <img src={UBC} alt="University of British Columbia Logo"/>
+          <div>
+            <h5 class="experience-title">Counterfactual Arguments Using Bayesian Networks</h5>
+            <p class="inline-block">I wrote a paper detailing an issue that was frequently overlooked when applying Judea Pearl’s theory with counterfactual arguments which is credited in David Poole and Alan Mackworth’s Artificial Intelligence textbook.</p>
+            <br/>
+            <p class="inline-block">You can read the paper </p> 
+            <a class="inline-block" href="./assets/COGS_402_Project.pdf" target="_blank">here</a> 
+            <p class="inline-block"> and see the textbook example </p>
+            <a class="inline-block" href="https://artint.info/AIPython/aipython/aipython.pdf#page=274">here.</a>
+          </div>
+          <img src={Vinquery} alt="VINquery Logo"/>
+          <div>
+            <h5 class="experience-title">VINfix Algorithm and RESTful API</h5>
+            <p class="inline-block">I designed the algorithm behind VINfix and the REST API that users and clients interface with.</p>
+            <br/>
+            <a class="inline-block" href="https://vinquery.com/vinfix">Here</a>
+            <p class="inline-block"> is an overview of what it does and </p>
+            <a class="inline-block" href="https://vinquery.com/docs-vinfix">here</a>
+            <p class="inline-block"> is the documentation with some sample return values from the API.</p>
+          </div>
+        </div>
+        <div class="end-image-container">
+          <img src={EndLight}/>
+        </div>
     </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     {/* <img src={logo} className="App-logo" alt="logo" /> */}
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     {/* <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a> */}
-    //   </header>
-    // </div>
   );
 }
 
--- personal-website/src/FloatingToolbar.js
+++ personal-website/src/FloatingToolbar.js
@@ -1,15 +1,16 @@
 // FloatingToolbar.js
 import React from 'react';
 import './FloatingToolbar.css'; // Import CSS for styling
+import MoonIcon from './assets/moon-icon.svg';
 
-const FloatingToolbar = () => {
+const FloatingToolbar = ({onScrollToHeader}) => {
   return (
     <div className="floating-toolbar">
       {/* <button>Home</button> */}
-      <button><img src="../moon-icon.svg" alt="Night Mode"></img></button>
-      <button>About Me</button>
-      <button>Experience/Education</button>
-      <button>Projects</button>
+      <button><img src={MoonIcon} alt="Night Mode"></img></button>
+      <button onClick={() => onScrollToHeader('header1')}>About Me</button>
+      <button onClick={() => onScrollToHeader('header2')}>Experience/Education</button>
+      <button onClick={() => onScrollToHeader('header3')}>Projects</button>
     </div>
   );
 };
